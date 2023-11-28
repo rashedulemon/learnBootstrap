15e46fee1e0d4492e8bde978485a8c712ff85961
Md. Rashedul Islam Emon <35653087+rashedulemon@users.noreply.github.com>
2023-11-28 21:42:52 +0600

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// this is a commend
+// console.log('Hello World');
+// console.log('Hello JS');
+
+let idNumber = 123;
+let firstName = 'Kareem';
+let lastName = 'Ali';
+let isSubmit = true;
+
+var n =3;
+
+const pi = 3.141592;
+
+
+let orderNumber = 21;
+let orderId =22;
+
+
+// variable name should not be a keyword of JS
+// variable name should not start with a number
+// variable name should be meaningful 
+
+idNumber = true;
+
+// console.log(p);
+console.log(firstName);
+console.log(isSubmit);
+
+
+let htxt = document.getElementById('hText');
+// let cbtn = documentg.getElementById('btn');
+
+function changeText() {
+    htxt.innerText = 'Hello Word';
+    console.log('Button Clicked');
+}
