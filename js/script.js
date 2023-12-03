9e5f05f031f89c461977d4e7391c2dcf09b6abb4
Md. Rashedul Islam Emon <35653087+rashedulemon@users.noreply.github.com>
2023-12-03 21:37:41 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,29 +2,29 @@
 // console.log('Hello World');
 // console.log('Hello JS');
 
-let idNumber = 123;
-let firstName = 'Kareem';
-let lastName = 'Ali';
-let isSubmit = true;
+// let idNumber = 123;
+// let firstName = 'Kareem';
+// let lastName = 'Ali';
+// let isSubmit = true;
 
-var n =3;
+// var n =3;
 
-const pi = 3.141592;
+// const pi = 3.141592;
 
 
-let orderNumber = 21;
-let orderId =22;
+// let orderNumber = 21;
+// let orderId =22;
 
 
 // variable name should not be a keyword of JS
 // variable name should not start with a number
 // variable name should be meaningful 
 
-idNumber = true;
+// idNumber = true;
 
 // console.log(p);
-console.log(firstName);
-console.log(isSubmit);
+// console.log(firstName);
+// console.log(isSubmit);
 
 
 // let cbtn = documentg.getElementById('btn');
@@ -34,41 +34,50 @@ console.log(isSubmit);
 
 
 
-let htxt = document.getElementById('hText');
-function changeText() {
-    htxt.innerText = Date();
-    console.log('Button Clicked');
+// let htxt = document.getElementById('hText');
+// function changeText() {
+//     htxt.innerText = Date();
+//     console.log('Button Clicked');
 
-}
+// }
 
 // Day 2
 
-z = multi(3,49);
+// z = multi(3,49);
 
-console.log(z);
+// console.log(z);
 
-function multi(x,y) {
-    let c = x*y;
-    return c;
+// function multi(x,y) {
+//     let c = x*y;
+//     return c;
+// }
+
+// greeting()
+
+// let x = 21;
+
+// greeting(x);
+
+// function greeting(a) {
+//     // const time = new Date().getHours();
+
+//     if (a<10) {
+//         console.log('a is under 0 to 10')
+//     } 
+//     else if (a<20) {
+//         console.log('a is under 10 to 20')
+//     }
+//     else{
+//         console.log('Greter then 20')
+//     }
+// }
+
+
+const pg = document.getElementById('para').innerHTML;
+const pg2 = document.getElementById('para1').innerHTML;
+// const pg3 = document.getElementsByClassName('p1').innerHTML;
+
+function rep(){
+    // document.getElementById('para1').innerHTML = pg;
+    document.getElementById("img1").style.width = "100%";
 }
-
-greeting()
-
-let x = 21;
-
-greeting(x);
-
-function greeting(a) {
-    // const time = new Date().getHours();
-
-    if (a<10) {
-        console.log('a is under 0 to 10')
-    } 
-    else if (a<20) {
-        console.log('a is under 10 to 20')
-    }
-    else{
-        console.log('Greter then 20')
-    }
-}
-
